c4ea09f530bc815adda7d712cacb1538e1761cd0
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-18 14:05:53 +0100

    Project initialization

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+  require 'User.php';
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Blog Users</title>
+  </head>
+  <body>
+    <div id="page-wrapper">
+      <div class="container">
+        <h1>Blog users</h1>
+        <h2>sottotitolo</h2>
+        <p>
+          <?php echo($prova); ?>
+        </p>
+      </div>
+    </div>
+  </body>
+</html>
