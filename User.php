2bb6f037d14f2fe663368ffdaff13df4a4860055
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-18 15:25:18 +0100

    "Users" table completed with variables types & attributes

--- User.php
+++ User.php
@@ -1,3 +1,10 @@
 <?php
+/* ASSIGNMENT
+  definire la struttura di una tabella users
+  che contiene i dati degli utenti di un blog
+  definire una classe User che "mappa" la tabella users
+  creare alcune istanze della classe User
+  per stampare i dati degli utenti
+*/
   $prova = 'prova';
 ?>
